1af5e18d6d7276b0c275411e50e6ca1f29a1580e
Nitin Gangwar <54510381+Nitin523@users.noreply.github.com>
2021-01-14 02:12:40 +0530

    Update urls.py

--- mentalcare/urls.py
+++ mentalcare/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+#Url routes
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hospital.urls')),
